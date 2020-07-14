bd2cfdf28e21d5693f0fb62c0a97932501376059
ocaoswaldo <68264932+ocaoswaldo@users.noreply.github.com>
2020-07-14 02:55:07 -0400

    Crear variables
    Se creo varias variables con su valor para realizar una operación, manteniendo las lineas de pruebas.

--- mi-archivo.js
+++ mi-archivo.js
@@ -1,2 +1,9 @@
 alert("EDITADO");
 console.log("cambios en el archivo");
+
+let precio = 100;
+let impuesto = 0.18;
+let descuento = 10;
+let precioFinal = precio + impuesto - descuento;
+
+console.log(precioFinal);
